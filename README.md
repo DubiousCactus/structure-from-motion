6a69f58c41b1466081b25981317347a126e8710d
Theo <theo.morales.fr@gmail.com>
2026-05-20 23:50:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# SfM from scratch
+
+## Roadmap
+
+### Numpy prototype
+- [ ] Feature Matching and Outlier rejection using RANSAC
+- [ ] Estimating Fundamental Matrix
+- [ ] Estimating Essential Matrix from Fundamental Matrix
+- [ ] Estimate Camera Pose from Essential Matrix
+- [ ] Check for Cheirality Condition using Triangulation
+- [ ] Perspective-n-Point
+- [ ] Bundle Adjustment
+
+### Taichi upgrade
---
+++ main.py
@@ -0,0 +1,311 @@
+import os
+from dataclasses import dataclass
+from typing import List, Optional, Sequence
+
+import cv2 as cv
+import numpy as np
+import typer
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+app = typer.Typer()
+
+
+@app.command()
+def extract_frames(video_path: str, output_folder: str):
+    cap = cv.VideoCapture(video_path)
+    os.makedirs(output_folder, exist_ok=True)
+    frame_count = 0
+    bar = tqdm(total=int(cap.get(cv.CAP_PROP_FRAME_COUNT)), desc="Extracting frames")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_filename = f"{output_folder}/frame_{frame_count:04d}.jpg"
+        cv.imwrite(frame_filename, frame)
+        frame_count += 1
+        bar.update(1)
+    bar.close()
+    cap.release()
+    print(f"Extracted {frame_count} frames to {output_folder}")
+
+
+@dataclass
+class ImageFeatures:
+    keypoints: List[cv.KeyPoint]
+    descriptors: List[np.ndarray]
+    img_path: str
+
+
+@dataclass
+class FrameTuple:
+    frame_a_id: int
+    frame_b_id: int
+    frame_a_features: ImageFeatures
+    frame_b_features: ImageFeatures
+    frame_a_to_b_matches: Sequence[cv.DMatch]
+
+
+class RANSAC:
+    def __init__(
+        self,
+        frame_tuples: List[FrameTuple],
+        consensus_ratio: float = 0.8,
+        max_iter: int = 1000,
+        threshold: float = 0.05,
+    ):
+        self.frame_tuples = frame_tuples
+        self.frame_inliers = []
+        self.consensus_min = len(frame_tuples) * consensus_ratio
+        self.max_iter = max_iter
+        self.threshold = threshold
+
+    def _compute_fundamental_matrix(
+        self, kp1: np.ndarray, kp2: np.ndarray
+    ) -> np.ndarray:
+        # INFO: The fundamental matrix F \in R^{3x3} algebraically represents the epipolar
+        # geometry that relates two sets of real-world point projections on two image
+        # planes. F gives us the epipolar constraint in the form (x_{i}^{\prime})^T F x_i = 0
+        assert len(kp1.shape) == 2 and kp1.shape[1] == 2
+        assert len(kp2.shape) == 2 and kp2.shape[1] == 2
+        # TODO: Hartley normalization. This may incur very poor performance due to huge
+        # dot products with large (u,v) coordinates.
+        A = []
+        for (x, y), (xp, yp) in zip(kp1, kp2):
+            A.append([xp * x, xp * y, xp, yp * x, yp * y, yp, x, y, 1])
+        A = np.asarray(A)
+        if not np.linalg.matrix_rank(A) >= 8:
+            raise ValueError("Bad design matrix, find better points.")
+        # Using SVD we can minimize min_{||f||=1} ||Xf||, where A is the design matrix for
+        # all point correspondences. The last column of V gives us the solution.
+        U, S, Vh = np.linalg.svd(
+            A
+        )  # Vh is the Hermitian transpose of V. For a real-valued matrix A, the hermitian transpose equals the normal transpose.
+        f_vec = Vh[-1, :]  # Last row of V^T is the last column of V
+        # WARN: However, due to noise in the observations, our solution does not fully
+        # minimize te objective, ie the last singular value is not exactly zero. This means
+        # that our optimal F is not rank 2 but full rank (3)! IE due to the rank-nullity
+        # theorem, the nullity of F is 0 and thus we haven't found the null space / solution
+        # to Ax=0. To enforce the rank 2 constraint, we can set the smallest singular value
+        # to zero and recompute F.
+        F = f_vec.reshape(3, 3)
+        U, S, Vh = np.linalg.svd(F)
+        S[-1] = 0  # Set smallest singular value to zero
+        F = U @ np.diag(S) @ Vh
+        return F
+
+    def filter(self):
+        """
+        Filter out outliers of a set of feature matches using RANSAC and the epipolar
+        constraint as condition. For each set of candidate matches, we compute the
+        Fundamental matrix and check for the epipolar constraint to be respected.
+        """
+        for f_tuple in self.frame_tuples:
+            inliers, best_fit, best_fit_err = None, None, np.inf
+            X_a = np.vstack(
+                [np.array(f.pt) for f in f_tuple.frame_a_features.keypoints]
+            )
+            X_b = np.vstack(
+                [np.array(f.pt) for f in f_tuple.frame_b_features.keypoints]
+            )
+            assert X_a.shape == X_b.shape
+            n_pts = X_a.shape[0]
+            X_a_homo = np.concatenate([X_a, np.ones((n_pts, 1))], axis=1)
+            X_b_homo = np.concatenate([X_b, np.ones((n_pts, 1))], axis=1)
+            pbar = tqdm(range(self.max_iter), desc="Finding inliers with RANSAC...")
+            for _ in pbar:
+                # 1. Select hypothetical outliers
+                # idx = np.random.permutation(n_pts)[:8]
+                idx = np.random.choice(n_pts, 8, replace=False)
+                x_a, x_b = X_a[idx], X_b[idx]
+                # 2. Fit a model to these
+                try:
+                    F = self._compute_fundamental_matrix(x_a, x_b)
+                except ValueError:
+                    continue
+                # 3. Test all data against this model. All data points that fit well are
+                # the consensus set (i.e. inliers).
+                # x^bFx^a with batch dimension:
+                # TODO: Use Sampson approximation instead for scale-independent error
+                err = np.abs((X_b_homo * (X_a_homo @ F.T)).sum(axis=1))
+                this_inliers = err < self.threshold
+                # 4. The model is reasonably good if sufficiently many points are
+                # classified as part of the consensus set.
+                if this_inliers.sum() < self.consensus_min:
+                    continue
+                this_err = err[this_inliers].mean()
+                pbar.set_postfix(
+                    {"#inliers": this_inliers.sum().item(), "Total err": this_err}
+                )
+
+                if this_err < best_fit_err:
+                    best_fit = self._compute_fundamental_matrix(
+                        X_a[this_inliers], X_b[this_inliers]
+                    )
+                    best_fit_err = this_err
+                    inliers = this_inliers
+                    # if this_err < self.threshold
+
+            self.frame_inliers.append(inliers)
+
+    def draw_matches(self):
+        for f, inliers in zip(self.frame_tuples, self.frame_inliers):
+            if inliers is None:
+                continue
+            matches = f.frame_a_to_b_matches
+
+            img1 = cv.imread(f.frame_a_features.img_path, cv.IMREAD_GRAYSCALE)
+            img2 = cv.imread(f.frame_b_features.img_path, cv.IMREAD_GRAYSCALE)
+            img = cv.drawMatches(
+                img1,
+                f.frame_a_features.keypoints,
+                img2,
+                f.frame_b_features.keypoints,
+                matches,
+                None,
+                matchColor=(0, 0, 255),
+                flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+            )
+
+            # Draw inliers in green
+            inlier_matches = [m for m, keep in zip(matches, inliers) if keep]
+
+            img = cv.drawMatches(
+                img1,
+                f.frame_a_features.keypoints,
+                img2,
+                f.frame_b_features.keypoints,
+                inlier_matches,
+                img,
+                matchColor=(0, 255, 0),
+                flags=cv.DrawMatchesFlags_DRAW_OVER_OUTIMG,
+            )
+
+            plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
+            plt.axis("off")
+            plt.show()
+
+
+class PosePredictor:
+    def _compute_essential_matrix(self):
+        raise NotImplementedError
+
+    def fit(self, points: np.ndarray):
+        """
+        Fit camera poses to points observed in two images via Perspective-n-Point.
+        """
+        raise NotImplementedError
+
+
+class Triangulator:
+    def _check_cheirality_condition(self):
+        raise NotImplementedError
+
+    def triangulate(self):
+        raise NotImplementedError
+
+
+class BundleAdjustment:
+    def adjust(self):
+        raise NotImplementedError
+
+
+@app.command()
+def extract_and_match(frames_path: str, debug: Optional[bool] = False):
+    # INFO: Stage 1: feature extraction using ORB
+    orb = cv.ORB_create()
+    frame_features = []
+    for i, frame_file in tqdm(
+        enumerate(sorted(os.listdir(frames_path))), desc="Extracting features"
+    ):
+        frame_path = os.path.join(frames_path, frame_file)
+        img = cv.imread(frame_path, cv.IMREAD_GRAYSCALE)
+        kp = orb.detect(img, None)
+        kp, des = orb.compute(img, kp)
+        frame_features.append(ImageFeatures(kp, des, frame_path))
+        if debug and i == 0:
+            img2 = cv.drawKeypoints(img, kp, None, color=(0, 255, 0), flags=0)
+            plt.imshow(img2), plt.show()
+
+    # INFO: Stage 2: KNN-based feature matching with FLANN
+    FLANN_INDEX_LSH = 6
+    index_params = dict(
+        algorithm=FLANN_INDEX_LSH,
+        table_number=6,  # 12
+        key_size=12,  # 20
+        multi_probe_level=1,
+    )  # 2
+    search_params = dict(checks=50)  # or pass empty dictionary
+    frame_tuples = []
+
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+    for i in range(1, len(frame_features)):
+        f1, f2 = frame_features[i - 1], frame_features[i]
+        matches = flann.knnMatch(f1.descriptors, f2.descriptors, k=2)
+        # INFO: we call knnMatch(query=f1, train=f2) and get:
+        # DMatch: .query_id, .train_id, .train_img_id, .distance
+        # For RANSAC -- and for the rest -- we'll want something more structured, ie:
+        # Match{ .img_a_id, .img_a_feature_id, .img_b_id, .img_b_feature_id, .dist }
+        # and a list of [Match], and a list of [Feature] but the ids must match.
+        # Or potentially something simpler, all packaged into a frame tuple:
+        # FrameTuple{ .frame_a_id, .frame_a_features, .frame_b_id, .frame_b_features, .matches_a_to_b, }
+
+        good_matches = []
+        for j, match in enumerate(matches):
+            if len(match) < 2:
+                continue
+            m, n = match
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+        frame_tuples.append(FrameTuple(i - 1, i, f1, f2, good_matches))
+
+        if debug and i < 10:
+            # Need to draw only good matches, so create a mask
+            matchesMask = [[0, 0] for i in range(len(matches))]
+
+            # ratio test as per Lowe's paper
+            for j, (m, n) in enumerate(matches):
+                if m.distance < 0.7 * n.distance:
+                    matchesMask[j] = [1, 0]
+
+            draw_params = dict(
+                matchColor=(0, 255, 0),
+                singlePointColor=(255, 0, 0),
+                matchesMask=matchesMask,
+                flags=cv.DrawMatchesFlags_DEFAULT,
+            )
+            img1 = cv.imread(f1.img_path, cv.IMREAD_GRAYSCALE)
+            img2 = cv.imread(f2.img_path, cv.IMREAD_GRAYSCALE)
+            img3 = cv.drawMatchesKnn(
+                img1, f1.keypoints, img2, f2.keypoints, matches, None, **draw_params
+            )
+            plt.imshow(img3)
+            plt.show()
+
+    # INFO: Stage 3: RANSAC outlier removal via the epipolar constraint.
+    # Now that we've got good candidate matches, we can start filtering them with
+    # RANSAC and the Fundamental matrix, ie if x'TFx ~= 0 the match is good, otherwise
+    # it's an outlier.
+    ransac = RANSAC(frame_tuples)
+    inliers = ransac.filter()
+    ransac.draw_matches()
+
+    # INFO: Stage 4: Camera pose prediction
+    poses = PosePredictor().fit(inliers)
+
+    # INFO: Stage 5: Check for Cheirality condition using Triangulation
+    _ = Triangulator().triangulate()
+
+    # INFO: Stage 6: Bundle adjustment
+    _ = BundleAdjustment().adjust()
+
+    # os.makedirs(".tmp", exist_ok=True)
+    # # np.savez(".tmp/matches.npz", frame_matches)
+    # with open(".tmp/matches.pkl", "wb") as f:
+    #     import pickle
+    #
+    #     pickle.dump(frame_matches, f)
+
+
+if __name__ == "__main__":
+    app()
